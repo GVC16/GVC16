7a2c1d252db9d5ec1cadb4d91ebc2d05877a1530
GVC16 <132665177+GVC16@users.noreply.github.com>
2024-07-31 17:22:05 +0000

    Conversion of Datatypes

---
+++ .devcontainer/01Basics/03Conversion_Operations.js
@@ -0,0 +1,20 @@
+let score = null;
+let valueOfScore = Number(score);
+// console.log(typeof(score));
+// console.log(typeof(valueOfScore));
+// console.log(valueOfScore);
+
+//null is a value having the datatype of Object
+// "33" --> converted to number and value=33
+// "33abc" --> converted to number but value=NaN
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 3567;
+let stringNumber = string(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
