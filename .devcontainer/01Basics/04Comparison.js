1b40490dfff4caed0a7ccd739586a11f8652d612
GVC16 <132665177+GVC16@users.noreply.github.com>
2024-08-11 10:30:39 +0000

    updated comparison with Stack and Heap memory

--- .devcontainer/01Basics/04Comparison.js
+++ .devcontainer/01Basics/04Comparison.js
@@ -2,8 +2,29 @@
 comparisons convert null to a number, treating it as 0.
 That's console.log(null>=0)--> true while console.log(null>0)--> false
 */
-console.log(null>0);
-console.log(null==0);
-console.log(null>=0);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
 
-console.log("2"===2);
+// console.log("2"===2);
+
+// Stack(Primitive); Heap(Non-Primitive)
+let myWebsiteName = "kuchtobhi.com";
+let anotherName = myWebsiteName;
+anotherName = "kuchbhitonahi.com";
+console.log(myWebsiteName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@gmail.com",
+    upiId: "user@ybl123"
+};
+let usertwo = userOne;
+usertwo.email = "user@yahoo.com";
+
+console.log(userOne.email);
+console.log(usertwo.email);
+
+
+
+
