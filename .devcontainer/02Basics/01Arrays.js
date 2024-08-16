bbde70b0c27e109baa1e562c847dd57cc85b46b2
GVC16 <132665177+GVC16@users.noreply.github.com>
2024-08-16 18:36:28 +0000

    Additional Array methods

--- .devcontainer/02Basics/01Arrays.js
+++ .devcontainer/02Basics/01Arrays.js
@@ -38,13 +38,45 @@ const newArray = myArray.join(); //converts array to the string
 //console.log(newArray); //returns array having string datatype
 
 // Slice and Splice
-console.log("A", myArray);
+// console.log("A", myArray);
 
 const mySlicedArray = myArray.slice(0, 3); // only creates the new array from original array, keeping original array intact
-console.log("B", mySlicedArray);
+// console.log("B", mySlicedArray);
 const mySplicedArray = myArray.splice(1, 3); // splices the original array and create new array making original array changed
-console.log("C", mySplicedArray); 
-console.log(myArray); //After Splice method the original array has been changed
+// console.log("C", mySplicedArray); 
+// console.log(myArray); //After Splice method the original array has been changed
+
+const marvelHeroes  = ["Thor", "spiderman", "Ironman"];
+const dcHeroes = ["Batman", "Superman", "Wonder_Woman"];
+
+// marvelHeroes.push(dcHeroes); // will add dcHeros array as an element in marvelheros array
+// console.log(marvelHeroes);
+
+const allSuperHeros = marvelHeroes.concat(dcHeroes); // Concat will create new array 
+// console.log(allSuperHeros);
+
+// using spread operator
+const allNewSuperHeros = [...marvelHeroes, ...dcHeroes]; //creates new array having all the elements of different arrays
+// console.log(allNewSuperHeros);
+
+const anotherArray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [3, 9]]];
+const usableAnotherArray = anotherArray.flat(2); // here parameter is depth create single array
+console.log(usableAnotherArray);
+
+//console.log(Array.isArray("Gaurav Chourasiya"));
+
+console.log(Array.from("234235")); // creates an array of given parameter. works well for String.
+console.log(Array.from({name: "Gaurav"})); // creates empty array cuz do not know whether create array using key or value.
+
+
+let scoreOne  = 100;
+let scoreTwo  = 200;
+let scoreThree = 300;
+console.log(Array.of(scoreOne, scoreTwo, scoreThree)); // creates new array with given parameters
+
+
+
+
 
 
  
