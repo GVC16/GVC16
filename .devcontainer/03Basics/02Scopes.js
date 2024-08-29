83b94d79e2ca49724813bde8d9922d2d01d3b437
GVC16 <132665177+GVC16@users.noreply.github.com>
2024-08-29 18:52:15 +0000

    JavaScript Functions

--- .devcontainer/03Basics/02Scopes.js
+++ .devcontainer/03Basics/02Scopes.js
@@ -6,11 +6,49 @@ if (true) {
     let a = 100;
     const b = 200;
     var c = 3000; // global effect
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
     
 }
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "hitesh";
+    function two(){
+        const website = "youtube";
+        //console.log(username);
+        
+    }
+    //console.log(website);
+    two();
+}
+one();
 
 
+//*************************************************************************************
+
+if(true){
+    const username = "Gaurav";
+    if (username === "Gaurav") {
+        const website = "youtube";
+        //console.log(username + website);
+    };
+    //console.log(website);
+};
+//console.log(username);
+
+//********************Interesting********************************
+addOne(5);
+
+function addOne(num){
+    return num + 1;
+};
+console.log(addOne(5));
+
+
+addTwo(5); 
+
+const addTwo = function(num){
+    return num + 2;
+};
