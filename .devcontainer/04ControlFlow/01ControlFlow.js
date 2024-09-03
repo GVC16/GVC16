139c5dc6c6fd6c29c8f60f97b93af048e835ad9e
GVC16 <132665177+GVC16@users.noreply.github.com>
2024-09-03 18:35:16 +0000

    conditionals

---
+++ .devcontainer/04ControlFlow/01ControlFlow.js
@@ -0,0 +1,49 @@
+//if-else conditionals
+
+const isUserLoggedIn = true;
+if(!isUserLoggedIn){
+    //console.log("Hello World!");
+}
+else{
+    //console.log("Hello Gaurav!");
+    
+}
+
+let temperature = 29;
+if(temperature > 40){
+    //console.log("Temp is greater than 29.");
+    
+}
+else{
+    //console.log("Temp is less than 29.");
+    
+};
+
+const score = 200;
+if (score >300) {
+    let power = "fly";
+    //console.log(`User power ${power}`);
+    
+} else {
+    let power = "not fly";
+    //console.log(power); 
+}
+
+//below: What not to do as standard practice of writing clean code although the code is correct
+// if(balance > 500) console.log("test1"), console.log("Test2");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
