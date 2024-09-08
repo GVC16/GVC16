6d12ea71d29dae2739b4f81168df4d0eb4d30ee8
GVC16 <132665177+GVC16@users.noreply.github.com>
2024-09-08 18:25:29 +0000

    more on iterators

--- .devcontainer/05Iterators/01Iterators.js
+++ .devcontainer/05Iterators/01Iterators.js
@@ -47,20 +47,62 @@ while (arrayIndex < myArray.length) {
 //     score++;
 // } while (score <= 10);
 
+const arr = [22, 33, 44, 55, 66, 77];
+for (const num of arr){
+    //console.log(num);
+}
+
+const greeting = "Hello World!";
+for (const greet of greeting) {
+    if (greet === " "){
+        continue;
+    }
+    //console.log(`Each char is ${greet}`);
+}
 
 
+ // Maps
+
+const map = new Map();
+map.set('IN', 'India');
+map.set('USA', 'United States of America');
+map.set('FR', 'France');
+map.set('IN', 'India'); // Map do not allow duplicate entries
+
+//console.log(map);
+for (const [key] of map) {
+    //console.log(key);
+    
+}
+for (const [key, value] of map) { // this will work
+    //console.log(key + ' : ' + value);
+}
 
 
+// const myobject = {
+//     game1 : 'NFS',
+//     game2 : 'Spiderman'
+// }
+// for (const [key, value] of myobject) { // this won't work
+//     //console.log(key + ' : ' + value);
+// }
 
+const myObject = {
+    js : 'JavaScript',
+    cpp : 'C++',
+    rb : 'Ruby',
+    swift : 'Swift by Apple'
+}
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
 
-
-
-
-
-
-
-
-
+for (const key in arr) {
+    //console.log(key);
+    console.log(`${key} : ${arr[key]}`);
+    
+}
 
 
 
